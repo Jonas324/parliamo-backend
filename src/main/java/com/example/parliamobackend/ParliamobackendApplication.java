43ca9c95e409c2fe67183c8164b81e0ddb289293
Jonas324 <jonaswadin24@gmail.com>
2023-01-10 13:53:21 +0100

    back  end is up

---
+++ src/main/java/com/example/parliamobackend/ParliamobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.parliamobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParliamobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParliamobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parliamobackend/ParliamobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parliamobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParliamobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
