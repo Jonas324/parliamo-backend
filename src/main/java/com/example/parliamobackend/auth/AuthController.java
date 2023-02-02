e991642777093fceb887e15d77c64a42d49061b2
OskarMartens <martens.oskar@gmail.com>
2023-02-02 16:49:56 +0100

    Add cross origin to authcontroller

--- src/main/java/com/example/parliamobackend/auth/AuthController.java
+++ src/main/java/com/example/parliamobackend/auth/AuthController.java
@@ -9,11 +9,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -38,11 +36,13 @@ public class AuthController {
 //        this.passwordEncoder = passwordEncoder;
 //    }
 
+    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
        return ResponseEntity.ok(authService.authenticate(request));
     }
 
+    @CrossOrigin
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
